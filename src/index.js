f9d02cb749498bef623bb5e902ab76e6295b8ba6
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-10-10 15:23:55 -0600

    Reconfigure code to update the date every 24 hrs

--- src/index.js
+++ src/index.js
@@ -2,10 +2,40 @@
 Overview of my solution:
 1. Create an array of cities on the page by grabbing them from the DOM.
 2. Feed the array of cities into a function that maps them to their timezones in the Moment JS dataset.
-3. Feed the array of formatted timezones into a function to update and format the time.
+3. Feed the array of formatted timezones into a function to update and format the date and time.
+4. UpdateDateTime bundles logic to updateDate and updateTime.
 4. The updateTime function uses setInterval to update the time per timezone associated with each city every second.
 NOTE: currently, the select form is not operational. This solution may be better suited for a search bar, which is less limited than a select menu.
 */
+const getCities = () => {
+  const cityElements = document.querySelectorAll(".city");
+  let cities = [];
+  cityElements.forEach((cityElement) => {
+    console.log(cityElement.childNodes[0].data);
+    const cityName = cityElement.childNodes[0].data;
+    const capitalizedCity =
+      cityName.charAt(0).toUpperCase() + cityName.slice(1); // Safeguard measure: ensure city is capitalized to facilitate search through timezones list
+    // console.log(capitalizedCity);
+    cities.push(capitalizedCity);
+  });
+  console.log(cities);
+  return cities;
+};
+
+const updateDate = (timezones) => {
+  const dateElements = document.querySelectorAll(".date");
+  // Establish the date right away, then trigger updates after 24 hours
+  dateElements.forEach((dateElement, index) => {
+    const date = moment().tz(`${timezones[index]}`).format("MMMM D, YYYY");
+    dateElement.innerHTML = date;
+  });
+  setInterval(() => {
+    dateElements.forEach((dateElement, index) => {
+      const date = moment().tz(`${timezones[index]}`).format("MMMM D, YYYY");
+      dateElement.innerHTML = date;
+    });
+  }, 86400000); // Update the date every 24 hours, which is 86400 * 1000 milliseconds
+};
 
 const updateTime = (timezones) => {
   const timeElements = document.querySelectorAll(".time");
@@ -17,6 +47,11 @@ const updateTime = (timezones) => {
       timeElement.innerHTML = time;
     });
   }, 1000);
+};
+
+const updateDateTime = (timezones) => {
+  updateDate(timezones);
+  updateTime(timezones);
 };
 
 const findTimezone = (cities) => {
@@ -33,23 +68,8 @@ const findTimezone = (cities) => {
     formattedTimezones.push(timezone);
   });
 
-  console.log(formattedTimezones);
-  updateTime(formattedTimezones);
-};
-
-const getCities = () => {
-  const cityElements = document.querySelectorAll(".city");
-  let cities = [];
-  cityElements.forEach((cityElement) => {
-    console.log(cityElement.childNodes[0].data);
-    const cityName = cityElement.childNodes[0].data;
-    const capitalizedCity =
-      cityName.charAt(0).toUpperCase() + cityName.slice(1); // Safeguard measure: ensure city is capitalized to facilitate search through timezones list
-    // console.log(capitalizedCity);
-    cities.push(capitalizedCity);
-  });
-  console.log(cities);
-  return cities;
+  // console.log(formattedTimezones);
+  updateDateTime(formattedTimezones);
 };
 
 const updateDisplay = () => {
